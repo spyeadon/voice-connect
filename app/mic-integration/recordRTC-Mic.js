d37a6abc5152f768637d26fb0664b6323c15620f
Soren Yeadon <spyeadon@gmail.com>
2017-03-13 12:22:05 -0500

    added more front-end functionality

--- app/mic-integration/recordRTC-Mic.js
+++ app/mic-integration/recordRTC-Mic.js
@@ -4,6 +4,11 @@ import axios from 'axios';
 const startButton = document.getElementById('btn-start-recording');
 const stopButton = document.getElementById('btn-stop-recording');
 const audio = document.getElementById('audioNode');
+const submitButton = document.getElementById('submitButton');
+const fromAddress = document.getElementById('fromAddress');
+const toAddress = document.getElementById('toAddress');
+const subjectLine = document.getElementById('subjectLine');
+const bodyContent = document.getElementById('bodyContent');
 
 let recordRTC = null;
 
@@ -25,21 +30,6 @@ stopButton.onclick = function () {
     recordRTC.stopRecording(function (audioWebMURL) {
         audio.src = audioWebMURL;
         console.log("audio src is: ", audio.src);
-
-        recordRTC.getDataURL(function(dataURL){
-          let recordedBlob = recordRTC.getBlob();
-          let encodedData = dataURL.split(',')[1];
-          let decodedData = window.atob(encodedData);
-          let encodedBlob = window.btoa(recordedBlob);
-          console.log("data URL is: ", dataURL);
-          console.log("recorded blob is: ", recordedBlob);
-          console.log("encoded data is: ", encodedData);
-          console.log("decoded data is: ", decodedData);
-
-          axios.post('/api/mail', {data: encodedData, length: length})
-          .then(res => console.log("response from server is: ", res.data))
-          .catch(err => console.error(err));
-        });
     });
 };
 
@@ -47,4 +37,38 @@ startButton.onclick = function () {
   navigator.mediaDevices.getUserMedia(session)
   .then(recording)
   .catch(error);
+}
+
+submitButton.onclick = function (event) {
+    let fromStr = fromAddress.value;
+    let toStr = toAddress.value;
+    let subLStr = subjectLine.value;
+    let bodyStr = bodyContent.value;
+
+    recordRTC.getDataURL(function(dataURL){
+      let recordedBlob = recordRTC.getBlob();
+      let encodedData = dataURL.split(',')[1];
+      let decodedData = window.atob(encodedData);
+      let encodedBlob = window.btoa(recordedBlob);
+      console.log("data URL is: ", dataURL);
+      console.log("recorded blob is: ", recordedBlob);
+      console.log("encoded data is: ", encodedData);
+      console.log("decoded data is: ", decodedData);
+
+      if (toStr && subLStr && bodyStr && fromStr) {
+        axios.post('/api/mail', {data: encodedData, fromAddress: fromStr, toAddress: toStr, subjectLine: subLStr, bodyContent: bodyStr })
+        .then(res => console.log("response from server is: ", res.data))
+        .then( () => {
+          fromAddress.value = '';
+          toAddress.value = '';
+          subjectLine.value = '';
+          bodyContent.value = '';
+        })
+        .catch(err => console.error(err));
+      }
+      else {
+        console.log('try again');
+      }
+    });
+
 }
